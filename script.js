49baf13e2a0cb4b396525cf5339de7e98a793f50
gleicyturismo <133262360+gleicyturismo@users.noreply.github.com>
2023-05-12 00:39:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Viagem", "Hospedagem", "Conforto e Segurança" ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
